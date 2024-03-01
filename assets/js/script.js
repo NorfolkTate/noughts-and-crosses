5b876008eb81ff26198475aaa8f8d9ff4fa5149a
Amy Norfolk Tate <amynorfolk2@gmail.com>
2024-03-01 09:06:54 +0000

    add win patterns

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,7 +27,16 @@ function makeMove(index) {
 }
 // Function to check a winner
 function checkWinner() {
-
+    let winPattern = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ];
 }
 // Function to reset game
 function resetGame() {
