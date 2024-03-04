16dffdc6d674539a93b2ebf08b3196bbd091a8c0
Amy Norfolk Tate <amynorfolk2@gmail.com>
2024-03-04 11:16:41 +0000

    remove old and incorrect code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,87 +1,68 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Game play
-    let currentPlayer = 'X';
-    let gameBoard = ['', '', '', '', '', '', '', '', ''];
-    let isGameActive = true;
-    let winner = null;
+// Game play
+let currentPlayer = 'X';
+let gameBoard = ['', '', '', '', '', '', '', '', ''];
+let isGameActive = true;
+let winner = null;
     
+let winPattern = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+];
+    
+const boxes = document.querySelectorAll('.box');
+const scoreElement = document.querySelector('.score');
+const resetButton = document.querySelector('.reset button');
+    
+    
+// Event listeners for boxes
+boxes.forEach((box, index) => {
+    box.addEventListener('click', () => makeMove(index));
+});
+    
+// Event listener for reset button
+resetButton.addEventListener('click', resetGame); 
+        
+// updated function to make a move
+function makeMove(index) {
+    if (gameBoard[index] === '' && !checkWinner()) {
+        gameBoard[index] = currentPlayer;
+        document.getElementsByClassName('box')[index].innerText = currentPlayer;
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        checkWinner();
+    }
+}
+        
+// Function to check a winner
+  function checkWinner() {
     let winPattern = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6],
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
     ];
     
-    const boxes = document.querySelectorAll('.box');
-    const scoreElement = document.querySelector('.score');
-    const resetButton = document.querySelector('.reset button');
-    
-    
-    // Event listeners for boxes
-    boxes.forEach((box, index) => {
-        box.addEventListener('click', () => makeMove(index));
-    });
-    
-    // Event listener for reset button
-    resetButton.addEventListener('click', resetGame);
-        
-    // updated function to make a move
-    function makeMove(index) {
-        if (gameBoard[index] === '' && !checkWinner()) {
-            gameBoard[index] = currentPlayer;
-            document.getElementsByClassName('box')[index].innerText = currentPlayer;
-            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-            checkWinner();
+    for (let pattern of winPattern) {
+        let [a, b, c] = pattern;
+        if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+            winner = gameBoard[a];
+            isGameActive = false;
+            displayWinner();
+            break;
         }
     }
-    
-    
-    /* Function to make a move
-    function makeMove(index) {
-        if (isGameActive && gameBoard[index] === '') {
-          gameBoard[index] = currentPlayer;
-          updateBoard();
-          checkForWinner();
-    
-          if (isGameActive) {
-            switchPlayer();
-            if (currentPlayer === 'O') {
-              // Computer's turn
-              setTimeout(makeComputerMove);
-            }
-          }
-      }
-    } */
-    
-    
-    // Function to check a winner
-    function checkWinner() {
-        let winPattern = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-        ];
-    
-        for (let pattern of winPattern) {
-            let [a, b, c] = pattern;
-            if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
-                winner = gameBoard[a];
-                isGameActive = false;
-                displayWinner();
-                break;
-            }
-        }
-    }
-    
+}
+
 // Function to reset game
 function resetGame() {
     currentPlayer = 'X';
@@ -94,12 +75,4 @@ function resetGame() {
 
     scoreElement.textContent = '';
 }
-
-boxes.forEach((box, index) => {
-    box.addEventListener('click', () => {
-        makeMove(index);
-    });
-});
-
-resetButton.addEventListener('click', resetGame);
 });
